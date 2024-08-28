44cd87e5049fd9c0e3454480b475a0f6e6a5d996
jianzquan <jianzq@stu.xmu.edu.cn>
2024-08-28 10:19:45 +0800

    init

---
+++ global_var.py
@@ -0,0 +1,13 @@
+
+
+## local paths
+root_dir = './' # 模型位置根目录
+data_dir = root_dir + 'datasets/' # 数据目录
+utils_dir = root_dir + 'utils/' # 训练代码位置目录
+
+## server paths
+plm_dir = '/home/Pretrained_Models/'
+cache_dir = '/home/jzq/Cache/'
+save_dir = './CheckPoints/'
+
+
